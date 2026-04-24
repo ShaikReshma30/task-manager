0cfa36480510fbc2da6406ba97b48ab49f426002
ShaikReshma <shaikreshma.career@gmail.com>
2026-04-25 02:15:48 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let tasks = [];
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  const task = req.body;
+  tasks.push(task);
+  res.json(task);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  const id = req.params.id;
+  tasks = tasks.filter((t, index) => index != id);
+  res.json({ message: "Deleted" });
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
